6f07bb20827288b795a02ebddfce0e1256c26183
Jeganathan, Vivin <vivin.jeganathan@usbank.com>
2019-07-21 17:54:31 -0700

    Initial Commit

---
+++ PanGestureSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PanGestureSample
+//
+//  Created by Jeganathan, Vivin on 7/21/19.
+//  Copyright © 2019 Jeganathan, Vivin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PanGestureSampleTests/PanGestureSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  PanGestureSampleTests.swift
+//  PanGestureSampleTests
+//
+//  Created by Jeganathan, Vivin on 7/21/19.
+//  Copyright © 2019 Jeganathan, Vivin. All rights reserved.
+//
+
+import XCTest
+@testable import PanGestureSample
+
+class PanGestureSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
